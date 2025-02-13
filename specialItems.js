81f635363a5738e721e7201026705a65556a65ed
Sebastian Wegmann <wegmase@de.ibm.com>
2025-02-13 18:34:28 +0100

    Correct spelling mistakes

--- specialItems.js
+++ specialItems.js
@@ -10,7 +10,7 @@ const sharedShield = [ "dusk forged", "sigil of protection", ];
 const commonArtefact = [ "talisman of shielding", "talisman of the void", "magic heirloom", "crown of the wizard king", "mimic cloak", ];
 const sharedArtefact = [ "essence of a free mind", "mask of mindless violence", "obsidian rock", ];
 const commonPotion = [ "binding scroll", "potion of power preservation", "dragon's brew", "potion of swiftness", ];
-const sharedPotion = [ "potion of healing", "scroll of draining", "scroll of power", "spell scroll (x)", ];
+const sharedPotion = [ "potion of healing", "scroll of draining", "scroll of power", "spell scroll", ];
 const commonBanner = [ "stalker's standard", "sheltering standard", "flaming standard", "aether icon", ];
 const sharedBanner = [ "banner of courage", "banner of speed", "distortion emblem", "rending banner" ];
 
@@ -151,7 +151,7 @@ const vsItems = [
 const wdgItems = [
     "burning portent", "symbol of slaughter",
     "thrice-forged", "gladiator's spirit",
-    "zealot's banner", "icon of the infinite", "wasteland torch",
+    "zealots' banner", "icon of the infinite", "wasteland torch",
     "veilgate orb", "dark familiar", "immortal gauntlets", "lord of the damned",
     "wyrd stone"
 ];
