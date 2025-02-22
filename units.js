9535cdfa24a87dd15ea2878af935a3a20ce9f63d
Sebastian Wegmann <sebastian.wegmann@gmx.net>
2025-02-22 12:00:21 +0100

    Add workaround and other multi-mode units

--- units.js
+++ units.js
@@ -708,6 +708,15 @@ const eos = [
     "min" : 15,
     "max" : 20,
   },
+  // Workaround for NR bug
+  {
+    "name" : "state detatchment",
+    "category" : "core",
+    "imperialAuxiliaries" : 0,
+    "imperialArmory" : 0,
+    "min" : 15,
+    "max" : 20,
+  },
   {
     "name" : "state marksmen",
     "category" : "core",
@@ -715,6 +724,31 @@ const eos = [
     "imperialArmory" : 0,
     "min" : 15,
     "max" : 20,
+  },
+  // Workaround for older EoS lists
+  {
+    "name" : "light infantry",
+    "category" : "core",
+    "imperialAuxiliaries" : 1,
+    "imperialArmory" : 0,
+    "min" : 15,
+    "max" : 20,
+  },
+  {
+    "name" : "heavy infantry",
+    "category" : "core",
+    "imperialAuxiliaries" : 0,
+    "imperialArmory" : 0,
+    "min" : 20,
+    "max" : 40,
+  },
+  {
+    "name" : "state militia",
+    "category" : "core",
+    "imperialAuxiliaries" : 1,
+    "imperialArmory" : 0,
+    "min" : 15,
+    "max" : 25,
   },
   {
     "name" : "local militia",
@@ -2408,6 +2442,20 @@ const vs = [
     "experimentalWeapon" : 0,
     "min" : 10,
     "max" : 30,
+  },
+  {
+    "name" : "fire rats",
+    "category" : "special",
+    "experimentalWeapon" : 0,
+    "min" : 10,
+    "max" : 20,
+  },
+  {
+    "name" : "bloodfur praetorians",
+    "category" : "special",
+    "experimentalWeapon" : 0,
+    "min" : 20,
+    "max" : 40,
   },
   {
     "name" : "rat swarm",
